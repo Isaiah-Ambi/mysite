ba6889aebfaa083485a9b5e0c7e0a8bd7fcc982e
Isaiah-Ambi <ambiisaiah24@gmail.com>
2024-04-02 12:14:27 +0100

    init

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+// const ejs = require('ejs');
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.use('/public',express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.render('index');
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about');
+})
+
+app.get('/education', (req, res) =>{
+    res.render('education');
+})
+
+app.get('/skills', (req, res) =>{
+    res.render('skills');
+})
+
+app.get('/projects', (req, res) =>{
+    res.render('projects');
+})
+
+app.get('/contact', (req, res) =>{
+    res.render('contact');
+})
+
+
+app.listen(3000,()=>{
+    console.log('listening')
+})
